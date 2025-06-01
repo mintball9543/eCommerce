61dc3f41cd3c54716b461b62244f9a817b8a71a0
mintball9543 <minseokcomputer@gmail.com>
2025-06-02 00:41:04 +0900

    feat : [006-CUSTOMER_balance] 고객에 대한 잔액 조회 구현

---
+++ src/main/java/com/example/cms/domain/model/CustomerBalanceHistory.java
@@ -0,0 +1,28 @@
+package com.example.cms.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerBalanceHistory extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(targetEntity = Customer.class, fetch = FetchType.LAZY)
+    private Customer customer;
+    // 변경된 돈
+    private Integer changeMoney;
+    // 해당 시점 잔액
+    private Integer currentMoney;
+
+    private String fromMessage;
+
+    private String description;
+
+}
