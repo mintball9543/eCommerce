52899461e7a5a67210c82d20b9775a05f9127221
mintball9543 <minseokcomputer@gmail.com>
2025-06-08 16:40:40 +0900

    feat : [014-SHOW-CART] Cart 보기 - 가격 / 수량 / 아이템 변경에 따른 대응

--- order-api/src/main/java/com/example/cms/order/service/ProductSearchService.java
+++ order-api/src/main/java/com/example/cms/order/service/ProductSearchService.java
@@ -24,7 +24,7 @@ public class ProductSearchService {
                 .orElseThrow(()->new CustomException(NOT_FOUND_PRODUCT));
     }
     public List<Product> getListByProductIds(List<Long> productIds){
-        return productRepository.findAllById(productIds);
+        return productRepository.findAllByIdIn(productIds);
     }
 
 
