eb21e1f32043668f595b778933abe2e37cdab1c3
mintball9543 <minseokcomputer@gmail.com>
2025-06-08 16:21:32 +0900

    feat : [012-Product-SEARCH] Product 에 대한 검색 기능 구현

---
+++ order-api/src/main/java/com/example/cms/order/service/ProductSearchService.java
@@ -0,0 +1,31 @@
+package com.example.cms.order.service;
+
+import com.example.cms.order.domain.model.Product;
+import com.example.cms.order.domain.repository.ProductRepository;
+import com.example.cms.order.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.example.cms.order.exception.ErrorCode.NOT_FOUND_PRODUCT;
+
+@Service
+@RequiredArgsConstructor
+public class ProductSearchService {
+    private final ProductRepository productRepository;
+
+    public List<Product> searchByName(String name){
+        return productRepository.searchByName(name);
+    }
+
+    public Product getByProductId(Long productId){
+        return productRepository.findWithProductItemsById(productId)
+                .orElseThrow(()->new CustomException(NOT_FOUND_PRODUCT));
+    }
+    public List<Product> getListByProductIds(List<Long> productIds){
+        return productRepository.findAllById(productIds);
+    }
+
+
+}
